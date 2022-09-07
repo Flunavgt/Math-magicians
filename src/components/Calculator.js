4003aa48f0ab64bbc7b8a3b9b46d8960f6da0acb
Federico Luna <fluna1973@yahoo.com>
2022-09-07 16:35:22 -0600

    corrected divide symbol

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable arrow-body-style */
-/* eslint-disable react/prop-types */
 import React from 'react';
 import '../Styles/style.css';
 import calculate from '../logic/calculate';
@@ -16,14 +13,12 @@ class Calculator extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  // eslint-disable-next-line class-methods-use-this
   handleClick(event) {
     const value = event.target.textContent;
     const solution = calculate(this.state, value);
     this.setState(solution);
   }
 
-  /* eslint-disable react/destructuring-assignment */
   render() {
     const { total, next, operation } = this.state;
     return (
@@ -41,27 +36,28 @@ class Calculator extends React.Component {
 }
 
 const KeyBoard = (props) => {
+  const { handler } = props;
   return (
     <div className="KeyBoard">
-      <button type="button" onClick={props.handler} className="btnPad0">0</button>
-      <button type="button" onClick={props.handler} className="btnPad1">1</button>
-      <button type="button" onClick={props.handler} className="btnPad2">2</button>
-      <button type="button" onClick={props.handler} className="btnPad3">3</button>
-      <button type="button" onClick={props.handler} className="btnPad4">4</button>
-      <button type="button" onClick={props.handler} className="btnPad5">5</button>
-      <button type="button" onClick={props.handler} className="btnPad6">6</button>
-      <button type="button" onClick={props.handler} className="btnPad7">7</button>
-      <button type="button" onClick={props.handler} className="btnPad8">8</button>
-      <button type="button" onClick={props.handler} className="btnPad9">9</button>
-      <button type="button" onClick={props.handler} className="btnPadAdd">+</button>
-      <button type="button" onClick={props.handler} className="btnPadMinus">-</button>
-      <button type="button" onClick={props.handler} className="btnPadDiv">+</button>
-      <button type="button" onClick={props.handler} className="btnPadMultiply">x</button>
-      <button type="button" onClick={props.handler} className="btnPadEqual">=</button>
-      <button type="button" onClick={props.handler} className="btnPadPercentage">%</button>
-      <button type="button" onClick={props.handler} className="btnPadAllClear">AC</button>
-      <button type="button" onClick={props.handler} className="btnPadMasMin">+/-</button>
-      <button type="button" onClick={props.handler} className="btnPadDecimal">.</button>
+      <button type="button" onClick={handler} className="btnPad0">0</button>
+      <button type="button" onClick={handler} className="btnPad1">1</button>
+      <button type="button" onClick={handler} className="btnPad2">2</button>
+      <button type="button" onClick={handler} className="btnPad3">3</button>
+      <button type="button" onClick={handler} className="btnPad4">4</button>
+      <button type="button" onClick={handler} className="btnPad5">5</button>
+      <button type="button" onClick={handler} className="btnPad6">6</button>
+      <button type="button" onClick={handler} className="btnPad7">7</button>
+      <button type="button" onClick={handler} className="btnPad8">8</button>
+      <button type="button" onClick={handler} className="btnPad9">9</button>
+      <button type="button" onClick={handler} className="btnPadAdd">+</button>
+      <button type="button" onClick={handler} className="btnPadMinus">-</button>
+      <button type="button" onClick={handler} className="btnPadDiv">÷</button>
+      <button type="button" onClick={handler} className="btnPadMultiply">x</button>
+      <button type="button" onClick={handler} className="btnPadEqual">=</button>
+      <button type="button" onClick={handler} className="btnPadPercentage">%</button>
+      <button type="button" onClick={handler} className="btnPadAllClear">AC</button>
+      <button type="button" onClick={handler} className="btnPadMasMin">+/-</button>
+      <button type="button" onClick={handler} className="btnPadDecimal">.</button>
     </div>
 
   );
