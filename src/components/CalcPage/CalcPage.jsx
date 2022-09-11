2ebc7dcdd38241ff1452d82d3e80aab958e6981a
Federico Luna <fluna1973@yahoo.com>
2022-09-11 00:25:15 -0600

    routes set up

---
+++ src/components/CalcPage/CalcPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from '../Calculator/Calculator';
+
+export default function CalcPage() {
+  return (
+    <div>
+      <div> Let&apos;s do some Math</div>
+      <div>
+        <Calculator />
+      </div>
+    </div>
+  );
+}
